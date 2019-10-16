5622510263c5e14e5fc735b66f0750fcff99c2a5
Mor Cohen <mor.cohen@microfocus.com>
2019-10-16 19:09:51 +0300

    Add to Result txt

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require('request')
 
+
+
 const forecast = (lat, long, callback) => {
     const url = `https://api.darksky.net/forecast/f37c193ff5632080aafd9bbfd207fa1a/${lat},${long}?units=si`
     request({ url, json: true }, (error, {body}) => {
@@ -10,7 +12,8 @@ const forecast = (lat, long, callback) => {
             callback("Unable to find location")
         }
         else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.'
+             + ' THE MAX TEMP IS ' + body.daily.data[0].apparentTemperatureHigh + ' THE MIN TEMP IS ' + body.daily.data[0].apparentTemperatureLow)
 
         }
     })
